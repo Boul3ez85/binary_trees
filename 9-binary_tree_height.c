61030d860a69b5c40c6d9f201524febfbae1eea0
Boul3ez85 <https://github.com/Boul3ez85>
2020-07-22 13:42:36 +0100

    height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - return the height of a given binary tree
+ * @tree: a pointer to a root node
+ *
+ * Return: return the height of a given binary tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t max_left, max_right, max = 0;
+
+	if (tree)
+	{
+		max_left = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+		max_right = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+
+
+		max += (max_left > max_right ? max_left : max_right);
+	}
+	return (max);
+}
