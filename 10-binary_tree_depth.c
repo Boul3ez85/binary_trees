0a0dc7e9a18ad0deeef52a647aa78aa0fe8a3214
Boul3ez85 <https://github.com/Boul3ez85>
2020-07-22 13:43:57 +0100

    depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - return the depth of a given binary tree
+ * @tree: a pointer to a root node
+ *
+ * Return: return the height of a given binary tree
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+
+	if (!tree)
+		return (0);
+
+	if (tree && tree->parent)
+	{
+		return (1 + binary_tree_depth(tree->parent));
+	}
+	return (0);
+}
