f76b25d883bd81a454baf2a1720fd28f2d20b319
Boul3ez85 <https://github.com/Boul3ez85>
2020-07-22 12:48:14 +0100

    insert a node

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - insert a new node at the left of a node
+ * @parent: a node to the root node
+ * @value: the value of the node to insert
+ *
+ * Return: a pointer to the new node
+ */
+
+binary_tree_t binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (!parent)
+		return (NULL);
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+
+	if (parent->left)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
+	parent->left = new;
+
+	return (new);
+}
